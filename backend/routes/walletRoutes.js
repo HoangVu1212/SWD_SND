e774be3200e225421512b50726070f9ed766a12c
Nguyen Truong Hoang Vu <hoangvu1212004@gmail.com>
2025-11-06 22:27:31 +0700

    Delete backend directory

--- backend/routes/walletRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { protect } = require("../middleware/authMiddleware");
-const { getBalance, addBalance } = require("../controllers/walletController");
-
-router.get("/me", protect, getBalance);   // Lấy thông tin ví
-router.post("/add", protect, addBalance); // Nạp tiền
-
-module.exports = router;
