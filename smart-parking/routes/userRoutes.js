3fb94ad30581424ba645a33082177a718acdd712
son-fpt <sonntde180881@fpt.edu.vn>
2025-11-11 09:02:34 +0700

    Delete smart-parking directory

--- smart-parking/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllUsers, deleteUser } = require("../controllers/userController");
-const { verifyToken, authorizeRole } = require("../middleware/authMiddleware");
-
-router.get("/", verifyToken, authorizeRole("admin"), getAllUsers);
-router.delete("/:id", verifyToken, authorizeRole("admin"), deleteUser);
-
-module.exports = router;
